8025b173adc2a06d7da3cd9e1409d0519ebe1a0f
bjwhip <bjwhip@gmail.com>
2017-09-24 12:08:52 -0600

    refactored

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,7 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
