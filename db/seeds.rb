824b552a2a423d3fa5aa3bd84fdaaf73271ff876
bjwhip <bjwhip@gmail.com>
2017-09-25 12:08:38 -0600

    implement enums

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,12 @@
 
 puts "1 User created"
 
+@admin = AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "1 Admin created"
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post + 1} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
